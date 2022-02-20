8cda0eb1121096b2e614e693ded755a2c6b9de85
darrenva <michaelmdvr@gmail.com>
2022-02-20 11:50:53 +0100

    btn-search

--- app_comicman/assets/js/js.js
+++ app_comicman/assets/js/js.js
@@ -1,15 +1,15 @@
 addEventListener('DOMContentLoaded' , ()=>{
-    const btn_menu= document.querySelector('.btn_menu');
-    if(btn_menu){
-        btn_menu.addEventListener('click', ()=>{
-            const menu_items=document.querySelector('.menu_items');
-            menu_items.classList.toggle('show');
-        });
-    }
+/********Eventos JavaScripts***********/
     const btn_darkmoder= document.querySelector('.btn_darkmoder');
     if(btn_darkmoder){
         btn_darkmoder.addEventListener('click', ()=>{
             document.body.classList.toggle('darkmoder');
+        });
+    }
+    const icon_search=document.querySelector('.icon-search');
+    if(icon_search){
+        icon_search.addEventListener('click', ()=>{
+            icon_search.classList.toggle('hide');
         });
     }
 });
