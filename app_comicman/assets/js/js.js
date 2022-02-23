2745eb39808d84f0f9ca115307e1873da9b924ee
darrenva <michaelmdvr@gmail.com>
2022-02-24 00:04:30 +0100

    working-slider

--- app_comicman/assets/js/js.js
+++ app_comicman/assets/js/js.js
@@ -7,7 +7,8 @@ addEventListener('DOMContentLoaded' , ()=>{
         });
     }
     callModalWindows_welcome();
-    Callslider();
+    let index=0;
+    index=callSlider(index);
 });
 
 /****
@@ -40,35 +41,21 @@ const callModalWindows_welcome= ()=>{
 };
 
 //Slider
-
-const Callslider=()=>{
-    //init
-    slides = document.getElementsByClassName("slide");
-    containerWidth = 500;
-
-    Array.prototype.forEach.call(slides, function (el, i) {//set the initial position of each slide
-    el.style.left = (i * containerWidth) + "px";
-    });
-
-    window.moveSlides = function (direction) {
-    run = true;
-    tracker = containerWidth; //500 in this example. We make a separate variable so we can decrement it
-
-    if (((direction == "next" && (parseInt(slides[0].style.left) <= (0 - (containerWidth * (slides.length - 1)))))) //compare against 2nd-to-last slide's index, otherwise it'll go 1 slide too far
-        || (direction == "prev" && (parseInt(slides[0].style.left) >= 0))) { run = false; }
-
-    if (run) {
-        var slideInterval = setInterval(function () {
-        moveRate = 4; //set the speed here (use numbers that the container's width can be divided by without a remainder)
-            Array.prototype.forEach.call(slides, function (el, i) {
-            if (tracker <= 0) {
-                clearInterval(slideInterval)
-            } else {
-                el.style.left = (direction == "next") ? (parseInt(el.style.left) - moveRate) + "px" : (parseInt(el.style.left) + moveRate) + "px";
-            }
-            });
-            tracker -= moveRate;
-        }, 1);
-    }
+const callSlider=(index)=>{
+    const images=['showcase-1','showcase-2','showcase-3'];
+    window.moveSlides=function(direction){
+        const header=document.querySelector('header');
+        if(direction=='next' && index<2){
+            index++;
+            header.setAttribute('class',images[index]);
+            return index;
+        }else if(direction=='prev' && index<2 && index>0){
+            index--;
+            header.setAttribute('class',images[index]);
+            return index;
+        }else{
+            index=0;
+            return index;
+        }
     }
 }
