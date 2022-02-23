23616532293a79b08349eb54040a5bf9b5e3a58c
darrenva <michaelmdvr@gmail.com>
2022-02-23 21:18:49 +0100

    working-slide

--- app_comicman/assets/js/js.js
+++ app_comicman/assets/js/js.js
@@ -6,8 +6,16 @@ addEventListener('DOMContentLoaded' , ()=>{
             document.body.classList.toggle('darkmoder');
         });
     }
-   
-    /*modal window welcome*/
+    callModalWindows_welcome();
+    Callslider();
+});
+
+/****
+ * Arrows Funtions
+ ****/
+
+//modal window welcome
+const callModalWindows_welcome= ()=>{
     let template_modal_window = `
     <div class="modal-window-welcome">
         <div class="modal-window-content">
@@ -29,6 +37,38 @@ addEventListener('DOMContentLoaded' , ()=>{
             modal_window_welcome.remove();
         });
     }
-    
-});
+};
 
+//Slider
+
+const Callslider=()=>{
+    //init
+    slides = document.getElementsByClassName("slide");
+    containerWidth = 500;
+
+    Array.prototype.forEach.call(slides, function (el, i) {//set the initial position of each slide
+    el.style.left = (i * containerWidth) + "px";
+    });
+
+    window.moveSlides = function (direction) {
+    run = true;
+    tracker = containerWidth; //500 in this example. We make a separate variable so we can decrement it
+
+    if (((direction == "next" && (parseInt(slides[0].style.left) <= (0 - (containerWidth * (slides.length - 1)))))) //compare against 2nd-to-last slide's index, otherwise it'll go 1 slide too far
+        || (direction == "prev" && (parseInt(slides[0].style.left) >= 0))) { run = false; }
+
+    if (run) {
+        var slideInterval = setInterval(function () {
+        moveRate = 4; //set the speed here (use numbers that the container's width can be divided by without a remainder)
+            Array.prototype.forEach.call(slides, function (el, i) {
+            if (tracker <= 0) {
+                clearInterval(slideInterval)
+            } else {
+                el.style.left = (direction == "next") ? (parseInt(el.style.left) - moveRate) + "px" : (parseInt(el.style.left) + moveRate) + "px";
+            }
+            });
+            tracker -= moveRate;
+        }, 1);
+    }
+    }
+}
